55e36a48af98a79c78fcce1b6f5000d86e733713
Williams Saya <williams.saya@fizzmod.com>
2023-08-24 14:24:55 -0300

    rama master

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-interface IcheckUpdateNeeded {
-    curVersion: string;
-    isAndroid?: boolean;
-    isDebug?: boolean;
-}
-declare const checkUpdateNeeded: ({ curVersion, isAndroid, isDebug, }: IcheckUpdateNeeded) => Promise<void>;
-export { checkUpdateNeeded };
